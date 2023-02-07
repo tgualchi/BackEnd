7c48b3274a3b9ac4ad4e26ebdb3a951b6ebf2f1d
tgualchi <97109000+tgualchi@users.noreply.github.com>
2023-02-07 06:31:00 -0300

    variable de base de datos2

--- src/main/java/com/porfolio/tob/Controller/CHys.java
+++ src/main/java/com/porfolio/tob/Controller/CHys.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
 @CrossOrigin(origins = {"https://frontendtg.web.app","http://localhost:4200"})
-@RequestMapping("/skill")
+@RequestMapping("/skill")//hys
 public class CHys {
 
     @Autowired
