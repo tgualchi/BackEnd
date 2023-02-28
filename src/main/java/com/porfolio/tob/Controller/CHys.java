36195f4e53e3b7697e543d64ddac19d6ea2ec9c1
tgualchi <97109000+tgualchi@users.noreply.github.com>
2023-02-28 16:56:43 -0300

    primer delpoyd

--- src/main/java/com/porfolio/tob/Controller/CHys.java
+++ src/main/java/com/porfolio/tob/Controller/CHys.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = {"https://frontendtg.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://centroboro-2df8c.web.app/","http://localhost:4200"})
 @RequestMapping("/skill")//hys
 public class CHys {
 
